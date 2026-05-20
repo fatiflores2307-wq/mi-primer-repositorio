6f9f82320b64ba0bc976821da53cc01f173cb10d
fatiflores2307-wq <fatiflores2307@gmail.com>
2026-05-20 13:57:05 -0600

    Datos de estudiantes

---
+++ estudiantes.py
@@ -0,0 +1,7 @@
+nombre = input("Escribe tu nombe aqui:")
+edad = input("Escribe tu edad aqui:")
+nie = input("Escribe tu nie aqui:")
+
+print =  ("Hola", nombre)
+print = ("Tienes", edad, "años")
+print = ("Este es tu nie", nie)
