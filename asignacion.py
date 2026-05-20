2e3220246189101a9c7e89ef9870f3483d443b8c
fatiflores2307-wq <fatiflores2307@gmail.com>
2026-05-20 15:52:24 -0600

    frutas

---
+++ asignacion.py
@@ -0,0 +1,16 @@
+x,y,z = "mango" , "sandia", "fresa"
+print(x)
+print(y)
+print(z)
+print("")
+x=Y=Z = "pera"
+print(x)
+print(y)
+print(z)
+print("")
+fruta = ["mango", "sandia", "fresa", "piña"]
+x,y,z , a = fruta
+print(x)
+print(y)
+print(z)
+print(a)
